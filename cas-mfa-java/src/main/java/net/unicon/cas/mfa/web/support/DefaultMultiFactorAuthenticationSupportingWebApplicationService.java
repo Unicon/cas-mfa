eab4426763f896199cd7252e3bfec4ead476c007
Andrew Petro <apetro@unicon.net>
2013-07-25 09:41:23 -0400

    refactor to move extraction into extractor Refactors to move ServletRequest-touching functionality for extracting information about a MfaService into the Extractor and out of the Service implementation.

--- cas-mfa-java/src/main/java/net/unicon/cas/mfa/web/support/DefaultMultiFactorAuthenticationSupportingWebApplicationService.java
+++ cas-mfa-java/src/main/java/net/unicon/cas/mfa/web/support/DefaultMultiFactorAuthenticationSupportingWebApplicationService.java
@@ -1,8 +1,5 @@
 package net.unicon.cas.mfa.web.support;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 
 import org.jasig.cas.authentication.principal.AbstractWebApplicationService;
@@ -11,7 +8,6 @@ import org.jasig.cas.authentication.principal.SimpleWebApplicationServiceImpl;
 import org.jasig.cas.util.HttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
 /**
  * The MultiFactorAuthenticationService is an extension of the generic CAS service
@@ -23,19 +19,17 @@ import org.springframework.util.StringUtils;
  *
  * @author Misagh Moayyed
  */
-public final class DefaultMultiFactorAuthenticationSupportingWebApplicationService extends AbstractWebApplicationService implements
-        MultiFactorAuthenticationSupportingWebApplicationService {
+public final class DefaultMultiFactorAuthenticationSupportingWebApplicationService
+        extends AbstractWebApplicationService
+        implements MultiFactorAuthenticationSupportingWebApplicationService {
 
     /** The logger instance. **/
-    protected static final Logger LOGGER = LoggerFactory.getLogger(DefaultMultiFactorAuthenticationSupportingWebApplicationService.class);
+    protected static final Logger LOGGER =
+            LoggerFactory.getLogger(DefaultMultiFactorAuthenticationSupportingWebApplicationService.class);
 
     private static final long serialVersionUID = 7537062414761087535L;
 
-    private static final String CONST_PARAM_SERVICE = "service";
 
-    private static final String CONST_PARAM_TARGET_SERVICE = "targetService";
-
-    private static final String CONST_PARAM_TICKET = "ticket";
 
     private final SimpleWebApplicationServiceImpl wrapperService;
 
@@ -46,12 +40,12 @@ public final class DefaultMultiFactorAuthenticationSupportingWebApplicationServi
      * Expects the request parameter {@link #CONST_PARAM_AUTHN_METHOD} is
      * present and its value is supported by the corresponding argument extractor.
      * @param id the service id
-     * @param originalUrl the service url from the request, noted by {@link #CONST_PARAM_SERVICE} or {@link #CONST_PARAM_TARGET_SERVICE}
-     * @param artifactId the artifact id from the request, noted by {@link #CONST_PARAM_TICKET}
+     * @param originalUrl the service url
+     * @param artifactId the artifact id
      * @param httpClient http client to process requests
-     * @param authnMethod the authentication method parameter defined for this mfa service
+     * @param authnMethod the authentication method required for this service
      */
-    protected DefaultMultiFactorAuthenticationSupportingWebApplicationService(final String id, final String originalUrl,
+    public DefaultMultiFactorAuthenticationSupportingWebApplicationService(final String id, final String originalUrl,
             final String artifactId, final HttpClient httpClient, @NotNull final String authnMethod) {
         super(id, originalUrl, artifactId, httpClient);
         this.wrapperService = new SimpleWebApplicationServiceImpl(id, httpClient);
@@ -67,45 +61,4 @@ public final class DefaultMultiFactorAuthenticationSupportingWebApplicationServi
         return this.authenticationMethod;
     }
 
-    /**
-     * Create an instance of {@link DefaultMultiFactorAuthenticationSupportingWebApplicationService} if loa
-     * parameter is defined and supported.
-     * @param request the http request
-     * @param httpClient the http client
-     * @param supportedLevelsOfAuthentication levels of mfa authentication supported by this service
-     * @return An instance of {@link DefaultMultiFactorAuthenticationSupportingWebApplicationService}
-     */
-    public static MultiFactorAuthenticationSupportingWebApplicationService createServiceFrom(final HttpServletRequest request,
-            final HttpClient httpClient, final List<String> supportedLevelsOfAuthentication) {
-        LOGGER.debug("Attempting to extract multifactor authentication parameters from the request");
-        final String targetService = request.getParameter(CONST_PARAM_TARGET_SERVICE);
-        final String authenticationMethod = request.getParameter(CONST_PARAM_AUTHN_METHOD);
-        final String serviceToUse = StringUtils.hasText(targetService) ? targetService : request.getParameter(CONST_PARAM_SERVICE);
-
-        if (!StringUtils.hasText(serviceToUse)) {
-            LOGGER.debug("Request has no request parameter [{}]", CONST_PARAM_SERVICE);
-            return null;
-        }
-
-        if (!StringUtils.hasText(authenticationMethod)) {
-            LOGGER.debug("Request has no request parameter [{}]", CONST_PARAM_AUTHN_METHOD);
-            return null;
-        }
-
-        if (!supportedLevelsOfAuthentication.contains(authenticationMethod)) {
-            LOGGER.debug("Multifactor authentication service does not support [{}] parameter value [{}].",
-                    CONST_PARAM_AUTHN_METHOD, authenticationMethod);
-            return null;
-        }
-
-        final String id = cleanupUrl(serviceToUse);
-        final String artifactId = request.getParameter(CONST_PARAM_TICKET);
-
-        final MultiFactorAuthenticationSupportingWebApplicationService svc =
-                new DefaultMultiFactorAuthenticationSupportingWebApplicationService(
-                id, serviceToUse, artifactId, httpClient, authenticationMethod);
-        LOGGER.debug("Created multifactor authentication request for [{}] with [{}] as [{}].",
-                svc.getId(), CONST_PARAM_AUTHN_METHOD, svc.getAuthenticationMethod());
-        return svc;
-    }
 }
--- cas-mfa-java/src/main/java/net/unicon/cas/mfa/web/support/MultiFactorAuthenticationArgumentExtractor.java
+++ cas-mfa-java/src/main/java/net/unicon/cas/mfa/web/support/MultiFactorAuthenticationArgumentExtractor.java
@@ -3,8 +3,12 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.jasig.cas.authentication.principal.WebApplicationService;
 import org.jasig.cas.web.support.AbstractSingleSignOutEnabledArgumentExtractor;
+import org.springframework.util.StringUtils;
 
 /**
  * The multifactor authentication argument extractor, responsible to
@@ -14,7 +18,27 @@ import org.jasig.cas.web.support.AbstractSingleSignOutEnabledArgumentExtractor;
  */
 public final class MultiFactorAuthenticationArgumentExtractor extends AbstractSingleSignOutEnabledArgumentExtractor {
 
+    /**
+     * The name of the request parameter conveying the service identifier.
+     */
+    public static final String CONST_PARAM_SERVICE = "service";
+
+    /**
+     * Alternative name of request parameter conveying the service identifier.
+     */
+    public static final String CONST_PARAM_TARGET_SERVICE = "targetService";
+
+    /**
+     * Name of the request parameter conveying the ticket identifier.
+     */
+    public static final String CONST_PARAM_TICKET = "ticket";
+
     private final List<String> supportedAuthenticationMethods;
+
+    /**
+     * This log would be replaced by the superclass's log if CAS-1332 realized.
+     */
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     /**
      * Create an instance of {@link MultiFactorAuthenticationArgumentExtractor}.
@@ -26,8 +50,68 @@ public final class MultiFactorAuthenticationArgumentExtractor extends AbstractSi
 
     @Override
     protected WebApplicationService extractServiceInternal(final HttpServletRequest request) {
-        return DefaultMultiFactorAuthenticationSupportingWebApplicationService.createServiceFrom(request,
-                getHttpClientIfSingleSignOutEnabled(), this.supportedAuthenticationMethods);
+
+        logger.debug("Attempting to extract multifactor authentication parameters from the request");
+        final String targetService = request.getParameter(CONST_PARAM_TARGET_SERVICE);
+        final String authenticationMethod =
+                request.getParameter(MultiFactorAuthenticationSupportingWebApplicationService.CONST_PARAM_AUTHN_METHOD);
+        final String serviceToUse =
+                StringUtils.hasText(targetService) ? targetService : request.getParameter(CONST_PARAM_SERVICE);
+
+        if (!StringUtils.hasText(serviceToUse)) {
+            logger.debug("Request has no request parameter [{}]", CONST_PARAM_SERVICE);
+            return null;
+        }
+
+        if (!StringUtils.hasText(authenticationMethod)) {
+            logger.debug("Request has no request parameter [{}]",
+                    MultiFactorAuthenticationSupportingWebApplicationService.CONST_PARAM_AUTHN_METHOD);
+            return null;
+        }
+
+        if (!supportedAuthenticationMethods.contains(authenticationMethod)) {
+            logger.debug("CAS is not configured to support [{}] parameter value [{}].",
+                    MultiFactorAuthenticationSupportingWebApplicationService.CONST_PARAM_AUTHN_METHOD,
+                    authenticationMethod);
+            return null;
+        }
+
+        final String id = exciseJsessionFromUrl(serviceToUse);
+        final String artifactId = request.getParameter(CONST_PARAM_TICKET);
+
+        final MultiFactorAuthenticationSupportingWebApplicationService svc =
+                new DefaultMultiFactorAuthenticationSupportingWebApplicationService(
+                        id, serviceToUse, artifactId, getHttpClientIfSingleSignOutEnabled(), authenticationMethod);
+        logger.debug("Created multifactor authentication request for [{}] with [{}] as [{}].",
+                svc.getId(), MultiFactorAuthenticationSupportingWebApplicationService.CONST_PARAM_AUTHN_METHOD,
+                svc.getAuthenticationMethod());
+        return svc;
     }
 
+    /**
+     * Duplicate of AbstractWebApplicationService.cleanupUrl(), which wasn't accessible to this extractor class.
+     * Excises out a jsession identifier from the service URL, if any.
+     * @param url String representation of URL potentially needing cleanup
+     * @return Cleaned up String representation of URL, or null
+     */
+    protected static String exciseJsessionFromUrl(final String url) {
+        if (url == null) {
+            return null;
+        }
+
+        final int jsessionPosition = url.indexOf(";jsession");
+
+        if (jsessionPosition == -1) {
+            return url;
+        }
+
+        final int questionMarkPosition = url.indexOf("?");
+
+        if (questionMarkPosition < jsessionPosition) {
+            return url.substring(0, url.indexOf(";jsession"));
+        }
+
+        return url.substring(0, jsessionPosition)
+                + url.substring(questionMarkPosition);
+    }
 }
