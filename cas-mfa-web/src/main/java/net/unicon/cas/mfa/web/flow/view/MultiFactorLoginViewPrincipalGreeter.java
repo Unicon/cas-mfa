64461a07aef73745f05102d41999110144ba0765
Misagh Moayyed <mmoayyed@unicon.net>
2013-08-07 13:25:03 -0700

    Fixed the issue where display names with comma in the value were rendered strangely.

--- cas-mfa-web/src/main/java/net/unicon/cas/mfa/web/flow/view/MultiFactorLoginViewPrincipalGreeter.java
+++ cas-mfa-web/src/main/java/net/unicon/cas/mfa/web/flow/view/MultiFactorLoginViewPrincipalGreeter.java
@@ -1,6 +1,7 @@
 package net.unicon.cas.mfa.web.flow.view;
 
 import org.jasig.cas.authentication.principal.Principal;
+import org.springframework.binding.message.MessageContext;
 
 /**
  * Defines an abstraction by which principals can be greeted in the view.
@@ -11,7 +12,8 @@ public interface MultiFactorLoginViewPrincipalGreeter {
      * Return the identifier that is to used to greet the credentials
      * in the view.
      * @param p the principal we are trying to welcome to the view.
+     * @param messageContext context to resolve the message code associated with the greeting
      * @return the greetee
      */
-    String getPersonToGreet(final Principal p);
+    String getPersonToGreet(final Principal p, MessageContext messageContext);
 }
