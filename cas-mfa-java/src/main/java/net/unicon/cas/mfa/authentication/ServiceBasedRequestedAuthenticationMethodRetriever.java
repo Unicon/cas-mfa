797299847cb6d3fa00231f7985084f834621c195
Dmitriy Kopylenko <dima767@gmail.com>
2014-05-18 13:45:24 -0400

    Initial definition of ServiceBasedRequestedAuthenticationMethodRetriever

---
+++ cas-mfa-java/src/main/java/net/unicon/cas/mfa/authentication/ServiceBasedRequestedAuthenticationMethodRetriever.java
@@ -0,0 +1,22 @@
+package net.unicon.cas.mfa.authentication;
+
+import org.jasig.cas.authentication.principal.WebApplicationService;
+
+/**
+ * Strategy interface defining a contract for retrieving requested authentication method based on target services
+ * acting as requesters for such additional authentication methods.
+ *
+ * @author Dmitriy Kopylenko
+ * @author Unicon inc.
+ */
+public interface ServiceBasedRequestedAuthenticationMethodRetriever {
+
+    /**
+     * Fetch authentication method from the given CAS service.
+     *
+     * @param webApplicationService target CAS service possibly encapsulating a request for an additional authentication method
+     *
+     * @return String representation of an authentication method is such exists or null otherwise.
+     */
+    String getAuthenticationMethodIfAny(WebApplicationService webApplicationService);
+}
