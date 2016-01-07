8faddbc342af53d42d2e7a298aaa63c609475923
Misagh Moayyed <mmoayyed@unicon.net>
2016-01-07 07:29:46 -0700

    fixed test failures

--- cas-mfa-java/src/test/groovy/net/unicon/cas/mfa/authentication/principal/PrincipalAttributeMultiFactorAuthenticationRequestResolverTests.groovy
+++ cas-mfa-java/src/test/groovy/net/unicon/cas/mfa/authentication/principal/PrincipalAttributeMultiFactorAuthenticationRequestResolverTests.groovy
@@ -1,13 +1,14 @@
 package net.unicon.cas.mfa.authentication.principal
 
+import groovy.util.logging.Slf4j
 import net.unicon.cas.mfa.authentication.AuthenticationMethod
 import net.unicon.cas.mfa.authentication.JsonBackedAuthenticationMethodConfigurationProvider
-import net.unicon.cas.mfa.web.support.MultiFactorWebApplicationServiceFactory
+import net.unicon.cas.mfa.web.support.DefaultMultiFactorWebApplicationServiceFactory
 import net.unicon.cas.mfa.web.support.MultiFactorAuthenticationSupportingWebApplicationService
-
-import static net.unicon.cas.mfa.web.support.MultiFactorAuthenticationSupportingWebApplicationService.AuthenticationMethodSource
+import net.unicon.cas.mfa.web.support.MultiFactorWebApplicationServiceFactory
 import org.jasig.cas.authentication.Authentication
 import org.jasig.cas.authentication.principal.Principal
+import org.jasig.cas.authentication.principal.Response
 import org.jasig.cas.authentication.principal.SimpleWebApplicationServiceImpl
 import org.jasig.cas.authentication.principal.WebApplicationService
 import spock.lang.Shared
@@ -15,12 +16,17 @@ import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
 
+import static net.unicon.cas.mfa.web.support.MultiFactorAuthenticationSupportingWebApplicationService.AuthenticationMethodSource
+
 /**
  *
  * @author Dmitriy Kopylenko
  * @author Unicon inc.
  */
+@Slf4j
 class PrincipalAttributeMultiFactorAuthenticationRequestResolverTests extends Specification {
+
+    static WebApplicationService targetService = new SimpleWebApplicationServiceImpl('test target service')
 
     @Shared
     def authenticationWithValidPrincipalAttributeFor_strong_two_factor = Stub(Authentication) {
@@ -37,23 +43,12 @@ class PrincipalAttributeMultiFactorAuthenticationRequestResolverTests extends Sp
         }
     }
 
-    @Shared
-    WebApplicationService targetService = new SimpleWebApplicationServiceImpl('test target service')
-
-    @Shared
-    MultiFactorWebApplicationServiceFactory mfaWebApplicationServiceFactory = Stub(MultiFactorWebApplicationServiceFactory) {
-        create('test target service', 'test target service', null, 'strong_two_factor', AuthenticationMethodSource.PRINCIPAL_ATTRIBUTE) >>
-                Stub(MultiFactorAuthenticationSupportingWebApplicationService) {
-                    getId() >> 'test target service'
-                    getAuthenticationMethod() >> 'strong_two_factor'
-                    getAuthenticationMethodSource() >> AuthenticationMethodSource.PRINCIPAL_ATTRIBUTE
-                }
-    }
+    MultiFactorWebApplicationServiceFactory mfaWebApplicationServiceFactory = new DefaultMultiFactorWebApplicationServiceFactory(true, null)
 
     @Subject
-    def s1 = [new AuthenticationMethod("strong_two_factor",1),
-              new AuthenticationMethod("lower_factor",2),
-              new AuthenticationMethod("lowest_factor",3)] as Set
+    def s1 = [new AuthenticationMethod("strong_two_factor", 1),
+              new AuthenticationMethod("lower_factor", 2),
+              new AuthenticationMethod("lowest_factor", 3)] as Set
 
     def loader = new JsonBackedAuthenticationMethodConfigurationProvider(s1)
 
@@ -63,7 +58,7 @@ class PrincipalAttributeMultiFactorAuthenticationRequestResolverTests extends Sp
     @Unroll
     def "either authentication OR service OR both null arguments OR no authn_method principal attribute SHOULD result in empty list"() {
         expect:
-        mfaAuthnReqResolverUnderTest.resolve(authn, svc).size() == 0
+        mfaAuthnReqResolverUnderTest.resolve(authn, svc, Response.ResponseType.REDIRECT).size() == 0
 
         where:
         authn                                                          | svc
@@ -74,11 +69,14 @@ class PrincipalAttributeMultiFactorAuthenticationRequestResolverTests extends Sp
 
     }
 
-    def "correct MultiFactorAuthenticationRequestContext returned when valid target service is passed and THERE IS a principal attribute 'authn_method'"() {
+    def "correct MultiFactorAuthenticationRequestContext returned when valid service is passed and a principal attribute 'authn_method'"() {
         given:
-        def mfaReq = mfaAuthnReqResolverUnderTest.resolve(authenticationWithValidPrincipalAttributeFor_strong_two_factor, targetService)
+
+        def mfaReq = mfaAuthnReqResolverUnderTest.resolve(authenticationWithValidPrincipalAttributeFor_strong_two_factor, targetService,
+                Response.ResponseType.REDIRECT)
         def mfaContext = mfaReq.get(0);
 
+        log.warn(mfaContext.mfaService.getId())
         expect:
         mfaContext.mfaService.id == 'test target service'
 
@@ -88,4 +86,5 @@ class PrincipalAttributeMultiFactorAuthenticationRequestResolverTests extends Sp
         and:
         mfaContext.mfaService.authenticationMethodSource == AuthenticationMethodSource.PRINCIPAL_ATTRIBUTE
     }
+
 }
--- cas-mfa-java/src/test/java/net/unicon/cas/mfa/web/support/RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractorTests.java
+++ cas-mfa-java/src/test/java/net/unicon/cas/mfa/web/support/RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractorTests.java
@@ -23,76 +23,77 @@ import static org.mockito.Mockito.*;
 public class RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractorTests {
 
     private static final String CAS_SERVICE = "https://mfa.cas.edu";
-    
+
     private static final String CAS_AUTHN_METHOD = "strong_two";
-    
-    private MultiFactorAuthenticationSupportingWebApplicationService getMfaService() {
+
+    private static MultiFactorAuthenticationSupportingWebApplicationService getMfaService() {
         return new DefaultMultiFactorAuthenticationSupportingWebApplicationService(CAS_SERVICE, CAS_SERVICE, null,
                 Response.ResponseType.REDIRECT, null, CAS_AUTHN_METHOD);
     }
-    
-    private HttpServletRequest getRequest() {
+
+    private static HttpServletRequest getRequest() {
         final HttpServletRequest req = mock(HttpServletRequest.class);
         when(req.getParameter(anyString())).thenReturn(CAS_SERVICE);
         return req;
-        
+
     }
-    
+
     @Test
     public void testServiceWithDefaultMfaAttribute() {
-        final List<ArgumentExtractor> set = new ArrayList<ArgumentExtractor>();
+        final List<ArgumentExtractor> set = new ArrayList<>();
         set.add(new CasArgumentExtractor());
-        
+
         final MultiFactorWebApplicationServiceFactory factory = mock(MultiFactorWebApplicationServiceFactory.class);
-        when(factory.create(anyString(), anyString(), anyString(), Response.ResponseType.REDIRECT,
+        when(factory.create(anyString(), anyString(), anyString(), any(Response.ResponseType.class),
                 anyString(), any(AuthenticationMethodSource.class)))
-            .thenReturn(getMfaService());
-        
+                .thenReturn(getMfaService());
+
         final AuthenticationMethodVerifier verifier = mock(AuthenticationMethodVerifier.class);
-        
-        final Map<String, Object> attrs = new HashMap<String, Object>();
+
+        final Map<String, Object> attrs = new HashMap<>();
         attrs.put(MultiFactorAuthenticationSupportingWebApplicationService.CONST_PARAM_AUTHN_METHOD, CAS_AUTHN_METHOD);
-        
+
         final RegisteredServiceWithAttributes svc = mock(RegisteredServiceWithAttributes.class);
         when(svc.getId()).thenReturn(0L);
         when(svc.getServiceId()).thenReturn(CAS_SERVICE);
         when(svc.getExtraAttributes()).thenReturn(attrs);
-        
+
         final ServicesManager mgmr = mock(ServicesManager.class);
         when(mgmr.findServiceBy(anyInt())).thenReturn(svc);
         when(mgmr.findServiceBy(any(Service.class))).thenReturn(svc);
-        
-        final RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor extractor = 
+
+        final RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor extractor =
                 new RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor(set, factory, mgmr, verifier);
 
         final MultiFactorAuthenticationSupportingWebApplicationService webSvc =
                 (MultiFactorAuthenticationSupportingWebApplicationService) extractor.extractService(getRequest());
+        assertNotNull(webSvc);
         assertEquals(webSvc.getAuthenticationMethod(), CAS_AUTHN_METHOD);
     }
-    
+
     @Test
     public void testServiceWithNoAttributeValue() {
-        final List<ArgumentExtractor> set = new ArrayList<ArgumentExtractor>();
+        final List<ArgumentExtractor> set = new ArrayList<>();
         set.add(new CasArgumentExtractor());
-        
+
         final MultiFactorWebApplicationServiceFactory factory = mock(MultiFactorWebApplicationServiceFactory.class);
         final AuthenticationMethodVerifier verifier = mock(AuthenticationMethodVerifier.class);
-        
-        final Map<String, Object> attrs = new HashMap<String, Object>();
+
+        final Map<String, Object> attrs = new HashMap<>();
         attrs.put(MultiFactorAuthenticationSupportingWebApplicationService.CONST_PARAM_AUTHN_METHOD, "");
-        
+
         final RegisteredServiceWithAttributes svc = mock(RegisteredServiceWithAttributes.class);
         when(svc.getId()).thenReturn(0L);
         when(svc.getServiceId()).thenReturn(CAS_SERVICE);
         when(svc.getExtraAttributes()).thenReturn(attrs);
-        
+
         final ServicesManager mgmr = mock(ServicesManager.class);
         when(mgmr.findServiceBy(anyInt())).thenReturn(svc);
         when(mgmr.findServiceBy(any(Service.class))).thenReturn(svc);
-        
-        final RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor extractor = 
+
+        final RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor extractor =
                 new RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor(set, factory, mgmr, verifier);
-        
+
         final MultiFactorAuthenticationSupportingWebApplicationService webSvc =
                 (MultiFactorAuthenticationSupportingWebApplicationService) extractor.extractService(getRequest());
         assertNull(webSvc);
@@ -100,17 +101,17 @@ public class RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtracto
 
     @Test
     public void testServiceWithMfaRole() {
-        final List<ArgumentExtractor> set = new ArrayList<ArgumentExtractor>();
+        final List<ArgumentExtractor> set = new ArrayList<>();
         set.add(new CasArgumentExtractor());
 
         final MultiFactorWebApplicationServiceFactory factory = mock(MultiFactorWebApplicationServiceFactory.class);
-        when(factory.create(anyString(), anyString(), anyString(), Response.ResponseType.REDIRECT,
+        when(factory.create(anyString(), anyString(), anyString(), any(Response.ResponseType.class),
                 anyString(), any(AuthenticationMethodSource.class)))
                 .thenReturn(getMfaService());
 
         final AuthenticationMethodVerifier verifier = mock(AuthenticationMethodVerifier.class);
 
-        final Map<String, Object> attrs = new HashMap<String, Object>();
+        final Map<String, Object> attrs = new HashMap<>();
         attrs.put(MultiFactorAuthenticationSupportingWebApplicationService.CONST_PARAM_AUTHN_METHOD, CAS_AUTHN_METHOD);
         attrs.put("mfa_role", new HashMap<String, Object>());
 
@@ -133,21 +134,21 @@ public class RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtracto
 
     @Test
     public void testServiceWithDifferentServiceType() {
-        final List<ArgumentExtractor> set = new ArrayList<ArgumentExtractor>();
+        final List<ArgumentExtractor> set = new ArrayList<>();
         set.add(new CasArgumentExtractor());
-        
+
         final MultiFactorWebApplicationServiceFactory factory = mock(MultiFactorWebApplicationServiceFactory.class);
         final AuthenticationMethodVerifier verifier = mock(AuthenticationMethodVerifier.class);
-        
+
         final RegisteredService svc = mock(RegisteredService.class);
         when(svc.getId()).thenReturn(0L);
         when(svc.getServiceId()).thenReturn(CAS_SERVICE);
-        
+
         final ServicesManager mgmr = mock(ServicesManager.class);
         when(mgmr.findServiceBy(anyInt())).thenReturn(svc);
         when(mgmr.findServiceBy(any(Service.class))).thenReturn(svc);
-        
-        final RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor extractor = 
+
+        final RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor extractor =
                 new RegisteredServiceAttributeMultiFactorAuthenticationArgumentExtractor(set, factory, mgmr, verifier);
 
         final MultiFactorAuthenticationSupportingWebApplicationService webSvc =
